3a1e7af7d806d9ff9c7f3071f0b0daaa2bf1866f
aarjanghimire <aarjanghimire.28@gmail.com>
2022-01-25 21:19:15 +0545

    front end

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,49 +1,29 @@
 
 <html>
-
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+<link href="./design.css" rel="stylesheet">
 <head>
-    <title>File Upload Progress Bar</title>
-    <style>
-        #bar_blank {
-            border: solid 1px #000;
-            height: 20px;
-            width: 300px;
-        }
-
-        #bar_color {
-            background-color: #006666;
-            height: 20px;
-            width: 0px;
-        }
-
-        #bar_blank,
-        #hidden_iframe {
-            display: none;
-        }
-        label {
-            background-color: indigo;
-            color: white;
-            padding: 0.5rem;
-            font-family: sans-serif;
-            border-radius: 0.3rem;
-            cursor: pointer;
-            margin-top: 1rem;
-        }
-    </style>
+    <title>PTSUSDT</title>
 </head>
 
 <body>
-    <div id="bar_blank">
-        <div id="bar_color"></div>
+    <div class="heading-text">
+        Upload Your Printing File Here
     </div>
-    <div id="status"></div>
     <form action="./AutoPrinter/upload.php" method="POST" id="myForm" enctype="multipart/form-data" target="hidden_iframe">
         
         <input type="hidden" value="myForm" name="<?php echo ini_get(" session.upload_progress.name"); ?>">
-        <input type="file" name="myFile" id="actual-btn">
-        <label for="actual-btn">Choose File</label><br>
-        <input type="submit" value="Start Upload">
+        <input type="file" name="myFile" id="upload-area">
+        <label for="upload-area">Browse file to Upload</label><br>
+        <div class="height"> </div>
+        <input type="submit" id="submit" value="Start Upload">
     </form>
+    <div id="status"></div>
+    <div id="bar_blank">
+        <div id="bar_color"></div>
+    </div>
     <iframe id="hidden_iframe" name="hidden_iframe" src="about:blank"></iframe>
     <script type="text/javascript" src="./script.js"></script>
 </body>
